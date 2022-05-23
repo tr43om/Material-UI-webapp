6d892289e565284ed5124697d2ee418f4a94f682
tr43om <tr43om@gmail.com>
2022-05-23 14:08:01 +0600

    ✨ Added text fields

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,15 +1,35 @@
-import { Typography, Button, Container } from "@mui/material";
+import { useState } from "react";
+import { Typography, Button, Container, TextField } from "@mui/material";
 import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
 
-const btn = {
-  fontSize: 60,
-  backgroundColor: "red",
-  "&:hover": {
-    backgroundColor: "blue",
-  },
-};
-
 export default function Create() {
+  const styles = {
+    btn: {
+      fontSize: "60px",
+      backgroundColor: "red",
+      "&:hover": {
+        backgroundColor: "blue",
+      },
+    },
+
+    field: {
+      marginTop: "2rem",
+      marginBottom: "2rem",
+      display: "block",
+    },
+  };
+
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
+
   return (
     <Container>
       <Typography
@@ -20,16 +40,43 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
-      <Button
-        // sx={btn}
-        type="submit"
-        color="secondary"
-        variant="contained"
-        onClick={() => console.log("clicked")}
-        endIcon={<KeyboardArrowRightOutlinedIcon />}
-      >
-        Submit
-      </Button>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          sx={styles.field}
+          label="Note title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <TextField
+          sx={styles.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          multiline
+          minRows={2}
+          maxRows={5}
+          required
+          value={details}
+          onChange={(e) => setDetails(e.target.value)}
+        />
+
+        <Button
+          // sx={styles.btn}
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightOutlinedIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
